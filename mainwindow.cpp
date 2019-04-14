03e824414e1c31bd8a49197fd743a467fb0828a4
liuxun <1541233570@qq.com>
2019-04-14 17:42:42 +0800

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,168 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_calButton_clicked()
+{
+    long long int firstvalue=ui->firstValue->text().toInt();
+    long long int secondvalue=ui->secondValue->text().toInt();
+    double value=-9999999;
+    int comboxvalue=ui->cboxButton->currentIndex();
+    switch (comboxvalue)
+    {
+    case 0:
+    {
+        value=firstvalue+secondvalue;
+            ui->myValue->setText(QString::number(value));
+    }
+        break;
+    case 1:
+        value=firstvalue-secondvalue;
+        ui->myValue->setText(QString::number(value));
+        break;
+    case 2:
+        value=firstvalue*secondvalue;
+        ui->myValue->setText(QString::number(value));
+        break;
+    case 3:
+    {
+        if(secondvalue==0)
+            ui->myValue->setText("除数不能为0");
+        else
+        {
+            value=(firstvalue*1.0)/secondvalue;
+            ui->myValue->setText(QString::number(value));
+        }
+    }
+        break;
+    }
+}
+
+void MainWindow::on_secondValue_returnPressed()
+{
+    long long int firstvalue=ui->firstValue->text().toInt();
+    long long int secondvalue=ui->secondValue->text().toInt();
+    double value=-9999999;
+    int comboxvalue=ui->cboxButton->currentIndex();
+    switch (comboxvalue)
+    {
+    case 0:
+    {
+        value=firstvalue+secondvalue;
+            ui->myValue->setText(QString::number(value));
+    }
+        break;
+    case 1:
+        value=firstvalue-secondvalue;
+        ui->myValue->setText(QString::number(value));
+        break;
+    case 2:
+        value=firstvalue*secondvalue;
+        ui->myValue->setText(QString::number(value));
+        break;
+    case 3:
+    {
+        if(secondvalue==0)
+            ui->myValue->setText("除数不能为0");
+        else
+        {
+            value=(firstvalue*1.0)/secondvalue;
+            ui->myValue->setText(QString::number(value));
+        }
+    }
+        break;
+    }
+}
+
+void MainWindow::on_firstValue_textChanged(const QString &arg1)
+{
+    ui->myValue->setText(" ");
+}
+
+void MainWindow::on_secondValue_textEdited(const QString &arg1)
+{
+    long long int firstvalue=ui->firstValue->text().toInt();
+    long long int secondvalue=ui->secondValue->text().toInt();
+    double value=-9999999;
+    int comboxvalue=ui->cboxButton->currentIndex();
+    switch (comboxvalue)
+    {
+    case 0:
+    {
+        value=firstvalue+secondvalue;
+            ui->myValue->setText(QString::number(value));
+    }
+        break;
+    case 1:
+        value=firstvalue-secondvalue;
+        ui->myValue->setText(QString::number(value));
+        break;
+    case 2:
+        value=firstvalue*secondvalue;
+        ui->myValue->setText(QString::number(value));
+        break;
+    case 3:
+    {
+        if(secondvalue==0)
+            ui->myValue->setText("除数不能为0");
+        else
+        {
+            value=(firstvalue*1.0)/secondvalue;
+            ui->myValue->setText(QString::number(value));
+        }
+    }
+        break;
+    }
+}
+
+void MainWindow::on_cboxButton_currentIndexChanged(int index)
+{
+    long long int firstvalue=ui->firstValue->text().toInt();
+    long long int secondvalue=ui->secondValue->text().toInt();
+    double value=-9999999;
+    int comboxvalue=ui->cboxButton->currentIndex();
+    switch (comboxvalue)
+    {
+    case 0:
+    {
+        value=firstvalue+secondvalue;
+        if(value==520)
+            ui->myValue->setText("何婷婷，我爱你呀！");
+        else
+            ui->myValue->setText(QString::number(value));
+    }
+        break;
+    case 1:
+        value=firstvalue-secondvalue;
+        ui->myValue->setText(QString::number(value));
+        break;
+    case 2:
+        value=firstvalue*secondvalue;
+        ui->myValue->setText(QString::number(value));
+        break;
+    case 3:
+    {
+        if(secondvalue==0)
+            ui->myValue->setText("笨蛋，除数不能为0");
+        else
+        {
+            value=(firstvalue*1.0)/secondvalue;
+            ui->myValue->setText(QString::number(value));
+        }
+    }
+        break;
+    }
+}
---
+++ mainwindow.h
@@ -0,0 +1,34 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#pragma execution_character_set("utf-8")
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_calButton_clicked();
+
+    void on_secondValue_returnPressed();
+
+    void on_firstValue_textChanged(const QString &arg1);
+
+    void on_secondValue_textEdited(const QString &arg1);
+
+    void on_cboxButton_currentIndexChanged(int index);
+
+private:
+    Ui::MainWindow *ui;
+
+};
+
+#endif // MAINWINDOW_H
